499cd72bb1230413f47e3188fd30030bba6c0033
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-04-11 15:08:15 +0300

    index.js updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client.js';
-import App from './components/App.js';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
